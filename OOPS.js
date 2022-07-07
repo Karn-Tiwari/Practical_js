16ee5c66f9f82e94a0e776fd4689dbd0091924bd
Karn Tiwari <karntiwari5353@gmail.com>
2022-07-07 16:26:37 +0530

    new commit

---
+++ OOPS.js
@@ -0,0 +1,9 @@
+// OOPS JS
+
+// Lear 'this' like never BeforeUnloadEvent
+// constructor function (create your own objects)
+
+// call apply bind methods
+
+// functon prototype and bind polyfill
+// prototypal inheritence

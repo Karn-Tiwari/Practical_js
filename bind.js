d1a0e04f09ee04551ac3642900b1c8feb5add79f
Karn Tiwari <karntiwari5353@gmail.com>
2022-07-08 12:00:12 +0530

    kahfd

--- bind.js
+++ bind.js
@@ -4,9 +4,21 @@ let person1 = {
     job: "teacher",
 }
 
-let showDetails = function (){
-    console.log(this.name + " " + this.age)
+let showDetails = function (city,state){
+    console.log(this.name + " " + this.age + " " + this.job + " " + city + " " + state)
 }
 
-let showDetailsBind = showDetails.bind(person1)
-showDetailsBind()
+let showDetailsBind = showDetails.bind(person1, 'Noida')
+showDetailsBind("UP")
+
+
+Function.prototype.myBind = function(...args){
+    let obj = this
+    params = args.slice(1)
+    return function(...args2){
+       obj.apply(args[0], [...params , ...args2])
+    }
+}
+
+let showDetailsMyBind = showDetails.myBind(person1,"noida")
+showDetailsMyBind("UP")

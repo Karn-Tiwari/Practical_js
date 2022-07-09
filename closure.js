88f86bb9202a368b19708be1881d2f4d048a9f41
Karn Tiwari <karntiwari5353@gmail.com>
2022-07-09 12:00:12 +0530

    kahfd

--- closure.js
+++ closure.js
@@ -1,35 +1,35 @@
-// // function calculate(a,b){
-// //     let result = a+b;
-// //     return result;
-// // }
-// // console.log(calculate(10,20));
-// // console.log(result)
-// // //function scope-> agar hum kis variable ko function k ander declare karte hai to uss variable ko bahar se access nhi kar sakte
-
-// function add(){
-//     let a = 4
-
-//     functionaddChild(){
-//         console.log(a+5)
-//     }
-//     addChild()
+// function calculate(a,b){
+//     let result = a+b;
+//     return result;
 // }
+// console.log(calculate(10,20));
+// console.log(result)
+// //function scope-> agar hum kis variable ko function k ander declare karte hai to uss variable ko bahar se access nhi kar sakte
 
-// add()
-// //lexical scope -> agar hum kisi child function ko function k ander declare karte hai to uss child functin k ander k variable ko access kar sakte hai uss parent function k ander se
+function add(){
+    let a = 4
+
+    functionaddChild(){
+        console.log(a+5)
+    }
+    addChild()
+}
+
+add()
+//lexical scope -> agar hum kisi child function ko function k ander declare karte hai to uss child functin k ander k variable ko access kar sakte hai uss parent function k ander se
 
 
-// // CLOSURE-> aesa function jiske pass access ho apne parent function k ander k variable ka chahe function return ho chuka ho ya even function bhi khatam ho chuka ho 
+// CLOSURE-> aesa function jiske pass access ho apne parent function k ander k variable ka chahe function return ho chuka ho ya even function bhi khatam ho chuka ho 
 
 
-// function add(){
-//     let a = 4
+function add(){
+    let a = 4
 
-//     functionaddChild(){
-//         console.log(a+5)
-//     }
-//    return addChild()
-// }
+    functionaddChild(){
+        console.log(a+5)
+    }
+   return addChild()
+}
 
-// let catchAdd = add()
-// console.log(catchAdd)
+let catchAdd = add()
+console.log(catchAdd)

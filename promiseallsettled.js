6c12331a93b77ef5f89225828a6e5ed16dc92aba
Karn Tiwari <karntiwari5353@gmail.com>
2022-07-14 15:30:21 +0530

    sdhjfg

--- promiseallsettled.js
+++ promiseallsettled.js
@@ -1,14 +1,14 @@
-// let p1 = new Promise(function(resolve, reject) {
-//     resolve('Promise 1 is resolved'); });
-// let p2 = new Promise(function(resolve, reject) {
-//     reject('Promise 2 is rejected'); }
-//     );
-// let p3 = new Promise(function(resolve, reject) {
-//     resolve('Promise 3 is resolved'); }
-//     );
+let p1 = new Promise(function(resolve, reject) {
+    resolve('Promise 1 is resolved'); });
+let p2 = new Promise(function(resolve, reject) {
+    reject('Promise 2 is rejected'); }
+    );
+let p3 = new Promise(function(resolve, reject) {
+    resolve('Promise 3 is resolved'); }
+    );
 
-//     Promise.allSettled([p1, p2, p3]).then((PromiseArr) => {
-//         console.log(PromiseArr);
-//     }).catch((err) => {
-//         console.log(err);
-//     })
+    Promise.allSettled([p1, p2, p3]).then((PromiseArr) => {
+        console.log(PromiseArr);
+    }).catch((err) => {
+        console.log(err);
+    })
